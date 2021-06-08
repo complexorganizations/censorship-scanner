fa7626fcff9bbcfae75fa53597f09659e39ab081
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-06-08 13:30:21 -0400

    Update main.go

--- main.go
+++ main.go
@@ -21,9 +21,8 @@ func init() {
 		flag.Parse()
 		basicScan = *tempBasicScan
 		advancedScan = *tempAdvancedScan
-	}
-	// Perform a basic scan if the user does not provide any instructions.
-	if !basicScan && !advancedScan {
+	} else {
+		// Perform a basic scan if the user does not provide any instructions.
 		basicScan = true
 	}
 	// Only perform one scan at a time.
@@ -58,16 +57,22 @@ func advancedNetworkCheck() {
 	websiteTestList := []string{
 		"https://www.google.com",
 		"https://github.com",
+		"https://www.af.mil",
 		"https://www.facebook.com",
+		"https://www.cia.gov",
 		"https://www.amazon.com",
+		"https://americorps.gov",
 		"https://www.youtube.com",
 		"https://www.yahoo.com",
 		"https://www.apple.com",
+		"https://www.nsa.gov",
 		"https://zoom.us",
 		"https://www.reddit.com",
 		"https://www.redtube.com",
 		"https://www.xtube.com",
 		"https://www.porn.com",
+		"https://www.fbi.gov",
+		"https://www.omct.org",
 		"https://www.msn.com",
 		"https://www.netflix.com",
 		"https://www.bing.com",
@@ -79,18 +84,32 @@ func advancedNetworkCheck() {
 		"https://wfrtds.org",
 		"https://www.who.int",
 		"https://www.tunnelbear.com",
+		"https://www.spacex.com",
 		"https://www.xswiper.com",
 		"https://www.pornhub.com",
 		"https://www.youporn.com",
 		"https://4genderjustice.org",
 		"https://www.xvideos.com",
+		"https://www.tesla.com",
 		"https://www.privateinternetaccess.com",
 		"https://www.prolife.com",
 		"https://www.office.com",
+		"https://tinder.com",
 		"https://www.hrw.org",
 		"https://www.twitch.tv",
 		"https://www.bbc.com",
 		"https://www.wikipedia.org",
+		"https://www.usa.gov",
+		"https://bumble.com",
+		"https://www.academyadmissions.com",
+		"https://www.state.gov",
+		"https://www.tsa.gov",
+		"https://www.whitehouse.gov",
+		"https://www.usds.gov",
+		"https://aal.army",
+		"https://www.federalreserveeducation.org",
+		"https://armyfuturescommand.com",
+		"https://www.nasa.gov",
 	}
 	// Start the test
 	for i := 0; i < len(websiteTestList); i++ {
@@ -99,9 +118,8 @@ func advancedNetworkCheck() {
 			log.Println("Failed: ", websiteTestList[i])
 		} else if !(websiteTestList[i] == resp.Request.URL.String()) {
 			log.Println("Error: ", websiteTestList[i])
-		} else {
-			fmt.Println("Passed: ", websiteTestList[i])
 		}
+		//else {	//fmt.Println("Passed: ", websiteTestList[i])	}
 		_ = resp
 	}
 }
