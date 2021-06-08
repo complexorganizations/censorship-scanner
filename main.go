bc143a4e699b490be533f8f1edb136f8933047fc
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-06-08 00:22:41 -0400

    Update main.go

--- main.go
+++ main.go
@@ -29,15 +29,37 @@ func basicNetworkCheck() {
 }
 
 func advancedNetworkCheck() {
+	// Lists of services to test
 	websiteTestList := []string{
 		"https://www.google.com",
 		"https://www.github.com",
+		"https://www.facebook.com",
+		"https://www.amazon.com",
 		"https://www.youtube.com",
+		"https://www.yahoo.com",
 		"https://www.apple.com",
+		"https://zoom.us",
+		"https://www.reddit.com",
+		"https://www.redtube.com",
+		"http://www.xtube.com",
+		"http://www.porn.com",
+		"https://www.msn.com",
+		"https://www.netflix.com",
+		"https://www.bing.com",
 		"https://www.microsoft.com",
 		"https://www.cloudflare.com",
+		"https://www.ebay.com",
+		"https://www.instagram.com",
+		"https://chaturbate.com",
+		"https://www.xswiper.com",
+		"http://www.pornhub.com",
+		"http://www.youporn.com",
+		"http://www.xvideos.com",
+		"https://www.office.com",
+		"https://www.twitch.tv",
 		"https://www.wikipedia.org",
 	}
+	// Start the test
 	for i := 0; i < len(websiteTestList); i++ {
 		resp, err := http.Get(websiteTestList[i])
 		if err != nil {
